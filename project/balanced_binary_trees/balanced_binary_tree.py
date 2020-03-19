1ef1f0ba6e91f240f61724d09f9772971a84339d
Anton Kovalev <acetonen@gmail.com>
2020-03-19 22:46:11 +0300

    GenerateBBSTArray

---
+++ project/balanced_binary_trees/balanced_binary_tree.py
@@ -0,0 +1,21 @@
+def recursively_fill_tree(index, array, result_tree):
+    if not array:
+        return
+
+    sub_index = len(array) // 2
+    result_tree[index] = array[sub_index]
+
+    left_side = array[:sub_index]
+    right_side = array[sub_index + 1:]
+    left_child = 2 * index + 1
+    right_child = 2 * index + 2
+
+    recursively_fill_tree(left_child, left_side, result_tree)
+    recursively_fill_tree(right_child, right_side, result_tree)
+
+
+def GenerateBBSTArray(array):
+    result_tree = [None for _ in array]
+
+    recursively_fill_tree(0, array, result_tree)
+    return result_tree
---
+++ project/balanced_binary_trees/tests/test_balanced_binary_tree.py
@@ -0,0 +1,11 @@
+from collections import namedtuple
+
+from project.balanced_binary_trees.balanced_binary_tree import GenerateBBSTArray
+
+TestFixture = namedtuple('TestFixture', 'list index')
+
+RESULT = [8, 4, 12, 2, 6, 10, 14, 1, 3, 5, 7, 9, 11, 13, 15]
+
+
+def test_generate_binary_array():
+    assert GenerateBBSTArray(list(range(1, 16))) == RESULT
