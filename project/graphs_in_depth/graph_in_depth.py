f73cf9c45c47d285064aba2e1ed6c199d791da88
Anton Kovalev <acetonen@gmail.com>
2020-04-05 22:50:36 +0300

    GraphInPepth

---
+++ project/graphs_in_depth/graph_in_depth.py
@@ -0,0 +1,80 @@
+# noinspection DuplicatedCode
+from typing import List
+
+
+class Vertex:
+    def __init__(self, value):
+        self.Value = value
+        self.Hit = False
+
+    def __repr__(self):
+        return str(self.Value)
+
+
+class SimpleGraph:
+    def __init__(self, size):
+        self.max_vertex = size
+        self.m_adjacency = [[0 for _ in range(size)] for _ in range(size)]
+        self.vertex = [None for _ in range(size)]
+
+    def AddVertex(self, vertex_value):
+        for index, place in enumerate(self.vertex):
+            if place is None:
+                self.vertex[index] = Vertex(vertex_value)  # noqa
+                break
+
+    def AddEdge(self, vertex1_index, vertex2_index):
+        self.m_adjacency[vertex1_index][vertex2_index] = 1
+        self.m_adjacency[vertex2_index][vertex1_index] = 1
+
+    def RemoveEdge(self, vertex1_index, vertex2_index):
+        self.m_adjacency[vertex1_index][vertex2_index] = 0
+        self.m_adjacency[vertex2_index][vertex1_index] = 0
+
+    def RemoveVertex(self, vertex_index):
+        for row in self.m_adjacency:
+            row[vertex_index] = 0
+
+        for column, _ in enumerate(self.m_adjacency[vertex_index]):
+            self.m_adjacency[vertex_index][column] = 0
+
+    def IsEdge(self, vertex1_index, vertex2_index):
+        return self.m_adjacency[vertex1_index][vertex2_index] == 1
+
+    def clean_vertex_hits(self):
+        for vertex in self.vertex:
+            if vertex is not None:
+                vertex.Hit = False
+
+    def _go_deeper(self, current_index, result_stack, to_index):
+        find = False
+        for index_two, edge_two in enumerate(self.m_adjacency[current_index]):
+            if edge_two == 1 and self.vertex[index_two].Hit is False:
+                find = self._try_to_find_from_near_vertexes(index_two, result_stack, to_index)
+
+        return find
+
+    def _search_nearest(self, current_index, to_index, result_stack):
+        for index_one, edge_one in enumerate(self.m_adjacency[current_index]):
+            if edge_one == 1 and index_one == to_index:
+                result_stack.append(self.vertex[index_one])  # noqa
+                return True
+
+    def _try_to_find_from_near_vertexes(self, current_index: int, result_stack: List[Vertex], to_index):
+        self.vertex[current_index].Hit = True
+        result_stack.append(self.vertex[current_index])  # noqa
+
+        find_nearest = self._search_nearest(current_index, to_index, result_stack)
+        if find_nearest:
+            return True
+
+        if not self._go_deeper(current_index, result_stack, to_index):
+            result_stack.pop()
+
+    def DepthFirstSearch(self, from_index: int, to_index: int):
+        result_stack = list()
+        self.clean_vertex_hits()
+
+        self._try_to_find_from_near_vertexes(from_index, result_stack, to_index)
+
+        return result_stack
---
+++ project/graphs_in_depth/tests/test_graph_in_depth.py
@@ -0,0 +1,27 @@
+from collections import namedtuple
+
+import pytest
+
+from project.graphs_in_depth.graph_in_depth import Vertex, SimpleGraph
+
+
+def create_vertexes_list_from_values(values_list):
+    return [Vertex(value) if value else None for value in values_list]
+
+
+def test_depth_first_search():
+    graph = SimpleGraph(5)
+    graph.vertex = create_vertexes_list_from_values([1, 2, 3, 4, 5])
+    for pair in [(0, 1), (1, 2), (0, 3), (3, 4)]:
+        graph.AddEdge(*pair)
+
+    assert [vertex.Value for vertex in graph.DepthFirstSearch(0, 4)] == [1, 4, 5]
+
+
+def test_depth_first_search_empty():
+    graph = SimpleGraph(5)
+    graph.vertex = create_vertexes_list_from_values([1, 2, 3, 4, 5, 6])
+    for pair in [(0, 1), (1, 2), (0, 3), (3, 4)]:
+        graph.AddEdge(*pair)
+
+    assert [vertex.Value for vertex in graph.DepthFirstSearch(0, 5)] == []
